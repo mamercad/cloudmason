da3c97f674efd911ae5d16a44b13c5fe71efcda0
Mark Mercado <mamercad@gmail.com>
2020-11-18 08:43:14 -0500

    Split off internal and external

--- octodns/sync.sh
+++ octodns/sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-eth2="$(../scripts/wan/eth2)" ../scripts/mo config/letsbuildthe.cloud.yaml.mo | tee config/letsbuildthe.cloud.yaml
+eth2="$(../scripts/wan/eth2)" ../scripts/mo config/external/letsbuildthe.cloud.yaml.mo | tee config/external/letsbuildthe.cloud.yaml
 octodns-sync --config-file config/production.yaml $*
