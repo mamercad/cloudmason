1bfb0db5320ad65ad7f77534405aeefb90cf9c07
Mark Mercado <mamercad@gmail.com>
2020-11-18 18:03:56 -0500

    Make this safer, fail fast

--- octodns/sync.sh
+++ octodns/sync.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-eth2="$(../scripts/wan/eth2)" ../scripts/mo config/external/letsbuildthe.cloud.yaml.mo | tee config/external/letsbuildthe.cloud.yaml
+
+export eth2="$(../scripts/wan/eth2)"
+export eth0="$(../scripts/wan/eth0)"
+
+if [[ -z "$eth2" ]] || [[ -z "$eth0" ]]; then
+  echo "Either eth2 or eth0 is empty, bailing!"
+  exit 1
+fi
+
+../scripts/mo config/external/cloudmason.org.yaml.mo | tee config/external/cloudmason.org.yaml
+../scripts/mo config/external/letsbuildthe.cloud.yaml.mo | tee config/external/letsbuildthe.cloud.yaml
+
 octodns-sync --config-file config/production.yaml $*
