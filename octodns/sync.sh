9588afafd42adf9d829771dd0dd5c6037e1bbcf0
Mark Mercado <mamercad@gmail.com>
2020-11-28 08:15:53 -0500

    Noping

--- octodns/sync.sh
+++ octodns/sync.sh
@@ -16,6 +16,6 @@ octodns-sync --config-file config/internal.yaml $*
 
 echo -n "Restart PowerDNS? "
 read ans
-ssh -v powerdns1 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"
+ssh -v net1 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"
 sleep 2
-ssh -v powerdns2 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"
+ssh -v net2 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"
