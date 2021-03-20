b11a2434ae9394e9f0b820f3c0d631470dbcb00a
Mark Mercado <mamercad@gmail.com>
2021-03-20 12:22:16 -0400

    Cleaning up OctoDNS

--- octodns/sync.sh
+++ octodns/sync.sh
@@ -1,22 +1,36 @@
 #!/usr/bin/env bash
 
-export eth2="$(../scripts/wan/eth2)"
-export eth0="$(../scripts/wan/eth0)"
+echo "Figuring out WAN addresses"
+
+eth2="$(../scripts/wan/eth2)"
+eth0="$(../scripts/wan/eth0)"
 
 if [[ -z "$eth2" ]] || [[ -z "$eth0" ]]; then
-  echo "Either eth2 or eth0 is empty, bailing!"
+  echo "$0 Either eth2 or eth0 is empty, bailing!"
   exit 1
+else
+  echo "eth2: $eth2"
+  echo "eth0: $eth0"
 fi
+
+echo "Templating external configs"
 
 ../scripts/mo config/external/cloudmason.org.yaml.mo | tee config/external/cloudmason.org.yaml
 ../scripts/mo config/external/letsbuildthe.cloud.yaml.mo | tee config/external/letsbuildthe.cloud.yaml
 
-octodns-sync --config-file config/external.yaml $*
-octodns-sync --config-file config/internal.yaml $*
+if ! command -v octodns-sync &>/dev/null; then
+  echo "$0: Can't find octodns-sync, bailing!"
+  exit 1
+fi
+
+echo "Syncing OctoDNS"
+
+octodns-sync --config-file config/external.yaml "$*"
+octodns-sync --config-file config/internal.yaml "$*"
+
+echo "Restarting PowerDNS"
 
 if [[ "$1" == "--doit" ]]; then
-  ssh net1 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"; sleep 3
-  ssh net2 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"; sleep 3
-  ssh net3 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"; sleep 3
-  ssh net4 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"; sleep 3
+  ssh mark@192.168.1.10 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"; sleep 3
+  ssh mark@192.168.1.11 "sudo systemctl restart pdns; sudo systemctl restart pdns-recursor"; sleep 3
 fi
